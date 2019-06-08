cdfbceef2e1a3a47542c142095262cb12364d29f
Roman Dzieciol <roman.dzieciol@gmail.com>
2019-06-08 14:40:48 +0100

    Upgrade to react-native 0.59.9

--- android/app/src/main/java/com/rntwitter/MainApplication.java
+++ android/app/src/main/java/com/rntwitter/MainApplication.java
@@ -3,10 +3,10 @@ package com.rntwitter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,10 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new RNScreensPackage(),
             new RNGestureHandlerPackage(),
-            new ReactNativeConfigPackage(),
-            new AsyncStoragePackage()
+            new ReactNativeConfigPackage()
       );
     }
 
